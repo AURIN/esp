242d2cfd69edeb8056fcb9d81abaeccc242ea927
Aram Kocharyan <akarmenia@gmail.com>
2014-06-27 13:15:21 +1000

    Moved app to /app to allow using bower and node with them being built by meteor. Added a grunt task to run install for bower, npm and grunt for both atlases and allow building atlas lazily. Added task to fix the style path in the built atlas-cesium to reference the path expected from the public directory.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,8 @@ module.exports = function(grunt) {
       shell = require('shelljs');
 
   var BOWER_DIR = 'bower_components';
-  var PUBLIC_DIR = 'public';
+  var APP_DIR = 'app';
+  var PUBLIC_DIR = appPath('public');
 
   var ATLAS_PATH = bowerPath('atlas');
   var ATLAS_BUILD_PATH = path.join(ATLAS_PATH, 'dist');
@@ -16,6 +17,11 @@ module.exports = function(grunt) {
   var ATLAS_CESIUM_BUILD_PATH = path.join(ATLAS_CESIUM_PATH, 'dist');
   var ATLAS_CESIUM_BUILD_FILE = path.join(ATLAS_CESIUM_BUILD_PATH, 'atlas-cesium.min.js');
   var ATLAS_CESIUM_RESOURCES_PATH = path.join(ATLAS_CESIUM_PATH, 'dist', 'cesium');
+  var ATLAS_CESIUM_STYLE_FILE = path.join(ATLAS_CESIUM_BUILD_PATH, 'atlas-cesium.min.css');
+
+  var bowerPaths = [ATLAS_PATH, ATLAS_CESIUM_PATH];
+  var npmPaths = [ATLAS_PATH, ATLAS_CESIUM_PATH];
+  var gruntPaths = [ATLAS_PATH, ATLAS_CESIUM_PATH];
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   // CONFIG
@@ -49,7 +55,56 @@ module.exports = function(grunt) {
   // INSTALL TASKS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  grunt.registerTask('install', 'Installs all dependencies', ['build-atlas', 'copy:resources']);
+  grunt.registerTask('install', 'Installs all dependencies', function () {
+    var args = arguments,
+        tasks = [],
+        addTasks = function() {
+          Array.prototype.slice.apply(arguments).forEach(function(task) {
+            tasks.push(task);
+          });
+        },
+        hasArgs = function(arg) {
+          return Object.keys(args).some(function(argIndex) {
+            var value = args[argIndex];
+            return value === arg;
+          });
+        };
+    addTasks('install-bower', 'install-npm', 'install-grunt');
+    addTasks('build-atlas' + (hasArgs('atlas-lazy') ? ':lazy' : ''));
+    addTasks('fix-atlas-build', 'copy:atlasResources');
+    console.log('Running tasks', tasks);
+    tasks.forEach(function(task) {
+      grunt.task.run(task);
+    })
+  });
+
+  grunt.registerTask('install-bower', 'Gets all bower dependencies', function() {
+    [''].concat(bowerPaths).forEach(function(dir) {
+      grunt.log.writeln(dir + ': running bower install');
+      shell.exec('cd ' + dir + ' && bower install');
+    });
+  });
+
+  grunt.registerTask('install-npm', 'Gets all node dependencies', function() {
+    npmPaths.forEach(function(dir) {
+      grunt.log.writeln(dir + ': running npm install');
+      shell.exec('cd ' + dir + '&& npm install --cache-min 999999999');
+    });
+  });
+
+  grunt.registerTask('install-grunt', 'Runs "grunt install" on all dependencies.', function() {
+    gruntPaths.forEach(function(dir) {
+      grunt.log.writeln(dir + ': running grunt install');
+      shell.exec('cd ' + dir + ' && grunt install');
+    });
+  });
+
+  grunt.registerTask('fix-atlas-build', 'Fixes the Atlas build.', function(arg1) {
+    // Replace the path to the cesium style which is now in the app's public folder.
+    writeFile(ATLAS_CESIUM_STYLE_FILE, function (data) {
+      return data.replace(/(@import\s+["'])(cesium)/, '$1atlas-cesium/$2');
+    });
+  });
 
   grunt.registerTask('build-atlas', 'Builds Atlas.', function(arg1) {
     var isLazy = arg1 === 'lazy';
@@ -63,11 +118,28 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.registerTask('meteor', 'Runs Meteor.', function() {
+    shell.exec('cd ' + APP_DIR + ' && meteor');
+  });
+
   grunt.registerTask('default', ['install']);
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   // AUXILIARY
   //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // FILES
+
+  function readFile(file) {
+    return fs.readFileSync(file, {encoding: 'utf-8'});
+  }
+
+  function writeFile(file, data) {
+    if (typeof data === 'function') {
+      data = data(readFile(file));
+    }
+    fs.writeFileSync(file, data);
+  }
 
   // PATHS
 
@@ -79,6 +151,10 @@ module.exports = function(grunt) {
 
   function bowerPath() {
     return _prefixPath(BOWER_DIR, arguments);
+  }
+
+  function appPath() {
+    return _prefixPath(APP_DIR, arguments);
   }
 
   function publicPath() {
